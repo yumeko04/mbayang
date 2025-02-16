37818ec25f45ee8e7328e3a3fd125011e1207adb
yumeko04 <fatouguinar63@gmail.com>
2025-02-16 17:39:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
 const app = express();
+const http = require('http');
 const server = http.createServer(app);
-const io = socketIo(server);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+// Servir les fichiers statiques (HTML, CSS, etc.)
+app.use(express.static(__dirname));
 
+// Gérer les connexions des clients
 io.on('connection', (socket) => {
     console.log('Un utilisateur est connecté');
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    socket.on('chat message', (data) => {
+        console.log('Message reçu : ', data);
+        io.emit('chat message', data);
     });
 
     socket.on('disconnect', () => {
-        console.log('Utilisateur déconnecté');
+        console.log('Un utilisateur est déconnecté');
     });
 });
 
+// Démarrer le serveur
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Le serveur fonctionne sur le port ${PORT}`);
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
+
+// Gérer proprement l'arrêt du serveur
+process.on('SIGINT', () => {
+    console.log('\nFermeture du serveur...');
+    server.close(() => {
+        console.log('Serveur fermé proprement.');
+        process.exit(0);
+    });
 });
